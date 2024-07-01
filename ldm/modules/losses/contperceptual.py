f1fef802394458322b59923436c0b0193582b573
Xingzhe Li <xngzhli@gmail.com>
2024-07-01 12:00:19 -0600

    custom lpips

--- ldm/modules/losses/contperceptual.py
+++ ldm/modules/losses/contperceptual.py
@@ -7,14 +7,14 @@ from taming.modules.losses.vqperceptual import *  # TODO: taming dependency yes/
 class LPIPSWithDiscriminator(nn.Module):
     def __init__(self, disc_start, logvar_init=0.0, kl_weight=1.0, pixelloss_weight=1.0,
                  disc_num_layers=3, disc_in_channels=3, disc_factor=1.0, disc_weight=1.0,
-                 perceptual_weight=1.0, use_actnorm=False, disc_conditional=False,
+                 perceptual_weight=1.0, use_actnorm=False, disc_conditional=False, lpips_kwargs={},
                  disc_loss="hinge"):
 
         super().__init__()
         assert disc_loss in ["hinge", "vanilla"]
         self.kl_weight = kl_weight
         self.pixel_weight = pixelloss_weight
-        self.perceptual_loss = LPIPS().eval()
+        self.perceptual_loss = LPIPS(**lpips_kwargs).eval()
         self.perceptual_weight = perceptual_weight
         # output log variance
         self.logvar = nn.Parameter(torch.ones(size=()) * logvar_init)
